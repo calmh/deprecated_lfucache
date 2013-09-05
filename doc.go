08a58a7c47b64a91e99c76a4853460958e866f51
Jakob Borg <jakob@nym.se>
2013-09-05 20:11:43 +0200

    Doc

--- doc.go
+++ doc.go
@@ -1,26 +1,29 @@
 /*
-Package lfucache implements an O(1) LFU cache structure.
+
+Package lfucache implements an O(1) LFU (Least Frequenty Used) cache.
 
 This structure is described in the paper "An O(1) algorithm for implementing
-the LFU cache eviction scheme" by  K. Shah, A. Mitra and D.  Matani.
+the LFU cache eviction scheme" by  K. Shah, A. Mitra and D.  Matani. It is
+based on two levels of doubly linked lists and gives O(1) insert, access and
+delete operations. It is implemented here with a few practical optimizations
+and extra features.
 
-It is based on two levels of doubly linked lists and gives O(1) insert, access
-and delete operations. The cache supports sending evicted items to interested
-listeners via channels, and manually evicting all cache items matching a
-criteria. This is useful for example when using the package as a write cache
-for a database, where items must be written to the backing store on eviction.
+The cache supports sending evicted items to interested listeners via channels,
+and manually evicting cache items matching a certain criteria. This is useful
+for example when using the package as a write cache for a database, where
+items must be written to the backing store on eviction.
 
-Example
+Example:
 
-	c := lfucache.Create(1024)
-	c.Access("mykey")       // => nil, false
-	c.Insert("mykey", 2345) // => true
-	c.Access("mykey")       // => interface{}{2345}, true
-	c.Delete("mykey")       // => true
+	c := lfucache.Create(1024) // The cache will hold up to 1024 items.
+	c.Access("mykey")          // => nil, false
+	c.Insert("mykey", 2345)    // => true
+	v, ok := c.Access("mykey") // => v = interface{}{2345}, ok = true
+	c.Delete("mykey")          // => true
 
-License
+---
 
-The MIT license.
+Copyright (c) 2013 Jakob Borg. Licensed under the MIT license.
 
 */
 package lfucache
